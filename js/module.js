caf3be74baa630fdeea6206251f6d1a7524d6114
Brajabandhu Das <brajabandhu.d@HCL.COM>
2017-07-24 17:04:46 +0530

    changes for json and http

--- js/module.js
+++ js/module.js
@@ -1,19 +1,25 @@
 var mymodule = angular.module("mymodule", ['ngRoute']);
-mymodule.config(['$routeProvider',function($routeProvider){
-$routeProvider.when('/home',{templateUrl:'views/home.html'})
-              .when('/directory',{templateUrl:'views/directory.html',controller:'mycontroller'})
-              .otherwise({redirectTo:'/home'});
+mymodule.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/home', { templateUrl: 'views/home.html' })
+                  .when('/directory', { templateUrl: 'views/directory.html', controller: 'mycontroller' })
+                  .otherwise({ redirectTo: '/home' });
 }]);
-var mycontroller = function ($scope) {
+var mycontroller = function ($scope,$http) {
     $scope.message = "This is my first Test";
     $scope.var = ['test1', 'test2', 'test3'];
-    $scope.arr = [
-        { name: "das", age: "29", salary: 33333, available: true },
-        { name: "bandhu", age: "25", salary: 29999, available: true },
-        { name: "braja", age: "30", salary: 20000, available: true },
-        { name: "pap", age: "27", salary: 19999, available: false }
+    // The below array is replaced by the below $http.get method which read data from json file, instead here creating manually so manythings.
+    
+    // $scope.arr = [
+    //     { name: "das", age: "29", salary: 33333, available: true },
+    //     { name: "bandhu", age: "25", salary: 29999, available: true },
+    //     { name: "braja", age: "30", salary: 20000, available: true },
+    //     { name: "pap", age: "27", salary: 19999, available: false }
+     
+    // ]
 
-    ]
+
+    // The below line is used to convert the above array into json format and display in console, then copy those  json data and put in a .json file
+    console.log(angular.toJson($scope.arr));
     // Removing an element from array.
     $scope.removeEle = function (loop2) {
         var val = $scope.arr.indexOf(loop2);
@@ -35,5 +41,8 @@ var mycontroller = function ($scope) {
 
 
     }
+$http.get('data/jsondata.json').success(function(data){
+    $scope.arr=data;
+});
 }
 mymodule.controller("mycontroller", mycontroller);
